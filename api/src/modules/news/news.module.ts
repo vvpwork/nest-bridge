babbc65cf40b5b6ac738eec164d85dab558cd3c0
Sasha Grin <aleksander.g@ideasoft.io>
2022-09-06 11:12:19 +0300

    BRID-889: fix for news endpoints

--- api/src/modules/news/news.module.ts
+++ api/src/modules/news/news.module.ts
@@ -5,10 +5,11 @@ import { NotificationService } from '@Modules/notification';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 import { News, NewsLike, Notification } from '@/db/models';
+import { AuthModule } from '@/modules';
 // import { AuthService } from '';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Notification, News, NewsLike])],
+  imports: [SequelizeModule.forFeature([Notification, News, NewsLike]), AuthModule],
   controllers: [NewsController],
   providers: [NewsService, NotificationService],
 })
